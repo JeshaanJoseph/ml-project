d9c209d698cd7405a6d2ddfde259666f73fe1820
JeshaanJoseph <jeshaanjoseph09@gmail.com>
2026-05-30 12:45:05 +0530

    refactor App component to implement prediction functionality and remove unused data fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [data, setData] = useState("");
+  const [income, setIncome] = useState("");
+  const [age, setAge] = useState("");
+  const [result, setResult] = useState("");
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/")
-      .then(res => res.json())
-      .then(res => setData(res.message));
-  }, []);
+  const handleSubmit = async () => {
+    const response = await fetch("http://127.0.0.1:8000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        income: Number(income),
+        age: Number(age),
+      }),
+    });
+
+    const data = await response.json();
+    setResult(data.prediction);
+  };
 
   return (
     <div>
-      <h1>React + FastAPI</h1>
-      <p>{data}</p>
+      <h1>ML Prediction App</h1>
+
+      <input
+        type="number"
+        placeholder="Income"
+        value={income}
+        onChange={(e) => setIncome(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="number"
+        placeholder="Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={handleSubmit}>
+        Predict
+      </button>
+
+      <h2>{result}</h2>
     </div>
   );
 }
