6132487d7b4124c0b1f0a3f14c35d1385c81b5f7
JeshaanJoseph <jeshaanjoseph09@gmail.com>
2026-05-30 21:56:03 +0530

    add prediction functionality and approval endpoint to FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import joblib
+import pandas as pd
 
 app = FastAPI()
+classifier = joblib.load("models/loan_classifier.pkl")
+scaler = joblib.load("models/scaler.pkl")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -23,8 +28,58 @@ def home():
 # prediction endpoint
 @app.post("/predict")
 def predict(data: InputData):
+
+    if data.income > 50000 and data.age > 21:
+        prediction = "Approved"
+    else:
+        prediction = "Rejected"
+
     return {
-        "received_income": data.income,
-        "received_age": data.age,
-        "prediction": "dummy result"
+        "prediction": prediction
+    }
+class LoanApprovalInput(BaseModel):
+    age: float
+    income: float
+    home_ownership: int
+    emplyment_length: float
+    loan_intent: int
+    loan_amount: float
+    loan_interest_rate: float
+    loan_income_ratio: float
+    payment_default_on_file: int
+    credit_history_length: float
+
+@app.post("/predict-approval")
+def predict_approval(data: LoanApprovalInput):
+
+    df = pd.DataFrame([[
+        data.age,
+        data.income,
+        data.home_ownership,
+        data.emplyment_length,
+        data.loan_intent,
+        data.loan_amount,
+        data.loan_interest_rate,
+        data.loan_income_ratio,
+        data.payment_default_on_file,
+        data.credit_history_length
+    ]], columns=[
+        'age',
+        'income',
+        'home_ownership',
+        'emplyment_length',
+        'loan_intent',
+        'loan_amount',
+        'loan_interest_rate',
+        'loan_income_ratio',
+        'payment_default_on_file',
+        'credit_history_length'
+    ])
+
+    scaled = scaler.transform(df)
+
+    prediction = classifier.predict(scaled)
+
+    return {
+        "approved": int(prediction[0])
     }
